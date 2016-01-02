ac90fb5357ef3b3c7688ed8713a659c99757b750
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-02 16:58:21 -0500

    3 more pixels of padding

--- js/console.js
+++ js/console.js
@@ -1,1 +1,1 @@
-console.info("%cThanks for visiting Knox Power!" , "background-color:#00fa12;border-radius:10px;padding:2px;");
+console.info("%cThanks for visiting Knox Power!" , "background-color:#00fa12;border-radius:10px;padding:5px;");
